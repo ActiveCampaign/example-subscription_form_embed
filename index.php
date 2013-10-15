246113ea78957014c5fc47ff106e1efc69521f0a
Matt Thommes <matt@activecampaign.com>
2013-10-15 13:00:43 -0500

    starting to add updates for loading an existing contact

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
-	define("ACTIVECAMPAIGN_URL", "");
-	define("ACTIVECAMPAIGN_API_KEY", "");
+	define("ACTIVECAMPAIGN_URL", "https://staging-mthommes.api-us1.com");
+	define("ACTIVECAMPAIGN_API_KEY", "3693354bb517da0392bd568c1cdcb2c5b2bd546d1dd1b373f82a7cda9850c04c2d126b9c");
 
 	require_once("../../activecampaign-api-php/includes/ActiveCampaign.class.php");
 	$ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
 
 	$form_embed_params = array(
-		"id" => 1026,
+		"id" => 1157,
 		"action" => "",
 		"ajax" => 0,
 		"css" => 1,
@@ -16,6 +16,16 @@
 	// perform sync (or swim? ;)
 	// if 0, it does an add/edit
 	$sync = 0;
+
+	function dbg($var, $continue = 0, $element = "pre") {
+	  echo "<" . $element . ">";
+	  echo "Vartype: " . gettype($var) . "\n";
+	  if ( is_array($var) ) echo "Elements: " . count($var) . "\n\n";
+	  elseif ( is_string($var) ) echo "Length: " . strlen($var) . "\n\n";
+	  print_r($var);
+	  echo "</" . $element . ">";
+		if (!$continue) exit();
+	}
 
 	$api_params = array();
 	foreach ($form_embed_params as $var => $val) {
@@ -28,6 +38,15 @@
 		// form submitted via ajax
 		echo $form_process;
 		exit;
+	}
+
+	// check for subscriber visiting this page (to preload their data).
+	$load_contact = false;
+	if (isset($_GET["hash"])) {
+		$load_contact = true;
+		$hash = $_GET["hash"];
+		$contact = $ac->api("contact/view?hash={$hash}");
+//dbg($contact);
 	}
 
 ?>
@@ -70,6 +89,28 @@
 
 		$form_html = $ac->api("form/embed?" . implode("&", $api_params));
 		echo $form_html;
+
+	?>
+
+	<?php
+	
+		if ($load_contact) {
+	
+			?>
+
+			<script>
+
+				$(document).ready(function() {
+
+					//$("#_form_<?php echo $form_embed_params["id"]; ?>")
+
+				});
+
+			</script>
+
+			<?php
+
+		}
 
 	?>
 
